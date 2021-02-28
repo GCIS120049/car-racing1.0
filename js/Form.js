d80996775cfa1645b281491cd818396cb1d82f12
GCIS120049 <aaravika2@gmail.com>
2021-02-28 11:58:02 +0530

    change

--- js/Form.js
+++ js/Form.js
@@ -4,6 +4,7 @@ class Form {
     this.input = createInput("Name");
     this.button = createButton('Play');
     this.greeting = createElement('h2');
+    this.button2 = createButton('reset');
   }
   hide(){
     this.greeting.hide();
@@ -14,10 +15,11 @@ class Form {
   display(){
     var title = createElement('h2')
     title.html("Car Racing Game");
-    title.position(130, 0);
+    title.position(displayWidth/2-100, 0);
 
-    this.input.position(130, 160);
-    this.button.position(250, 200);
+    this.input.position(displayWidth/2-80, displayHeight/2-80);
+    this.button.position(displayWidth/2-15, displayHeight/2);
+    this.button2.position(displayWidth-100,displayHeight/2-displayHeight);
 
     this.button.mousePressed(()=>{
       this.input.hide();
@@ -28,7 +30,7 @@ class Form {
       player.update();
       player.updateCount(playerCount);
       this.greeting.html("Hello " + player.name)
-      this.greeting.position(130, 100);
+      this.greeting.position(displayWidth/2-40,displayHeight/4);
     });
 
   }
